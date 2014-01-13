7d4b4008bdf2492a8fa5f6272a06684cb352d365
Monica Dragan <monique@work.(none)>
2014-01-13 09:56:33 +0100

    methods for arrays

---
+++ lib/genevalidator/enumerable.rb
@@ -0,0 +1,35 @@
+
+# extention of the enumerable module (i.e new methods fo vectors)
+module Enumerable
+
+  def sum
+    return self.inject(0){|accum, i| accum + i }
+  end
+
+  def mean
+    return self.sum / self.length.to_f
+  end
+
+  def median
+    sorted = self.sort
+    len = sorted.length
+    return (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+  end
+
+  def mode
+    freq = self.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+    self.sort_by { |v| freq[v] }.last
+  end
+
+  def sample_variance
+    m = self.mean
+    sum = self.inject(0){|accum, i| accum + (i - m) ** 2 }
+    return sum / (self.length - 1).to_f
+  end
+
+  def standard_deviation
+    return Math.sqrt(self.sample_variance)
+  end
+
+end
+
