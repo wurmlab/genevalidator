ed93ed03d814bff08c83a09317ef021e8cb6acf3
IsmailM <ismail.moghul@gmail.com>
2014-09-29 09:13:00 +0100

    update

--- lib/genevalidator/sequences.rb
+++ lib/genevalidator/sequences.rb
@@ -10,8 +10,8 @@ class Sequence
   attr_accessor :accession_no
   attr_accessor :length_protein
   attr_accessor :reading_frame
-  attr_accessor :hsp_list # array of Hsp objects 
- 
+  attr_accessor :hsp_list # array of Hsp objects
+
   attr_accessor :raw_sequence
   attr_accessor :protein_translation # used only for nucleotides
   attr_accessor :nucleotide_rf #used only for nucleotides
@@ -26,13 +26,13 @@ class Sequence
   def protein_translation
     if @type == :protein
       return raw_sequence
-    else 
+    else
       return @protein_translation
     end
   end
 
   ##
-  # Gets raw sequence by fasta identifier from a fasta index file 
+  # Gets raw sequence by fasta identifier from a fasta index file
   # Params:
   # +raw_seq_file+: name of the fasta file with raw sequences
   # +index_file_name+: name of the fasta index file
@@ -46,7 +46,7 @@ class Sequence
       if hash == nil
         hash = YAML.load_file(index_file_name)
       end
-   
+
       idx = hash[identifier]
 
       query         = IO.binread(raw_seq_file, idx[1] - idx[0], idx[0])
@@ -67,22 +67,23 @@ class Sequence
   # String with the nucleotide sequence corresponding to the accno
   def get_sequence_by_accession_no(accno, dbtype, db)
     begin
-      if (db !~ /-remote$/)
+      puts db
+      if (db !~ /remote/)
         blast_cmd     = "blastdbcmd -target_only -entry #{accno} -db #{db} -outfmt %f"
         seq           = %x[#{blast_cmd}  2>&1]
         @raw_sequence = seq.gsub!(/\n/,'')
       else
         #puts "Tries to connect to the internet for #{accno}"
-        uri = "http://www.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=#{db}"<<
+        uri = "http://www.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=#{dbtype}"<<
            "&retmax=1&usehistory=y&term=#{accno}/"
-        result   = Net::HTTP.get(URI.parse(uri))
+        result = Net::HTTP.get(URI.parse(uri))
 
         result2  = result
         queryKey = result2.scan(/<\bQueryKey\b>([\w\W\d]+)<\/\bQueryKey\b>/)[0][0]
         webEnv   = result.scan(/<\bWebEnv\b>([\w\W\d]+)<\/\bWebEnv\b>/)[0][0]
 
         uri = "http://www.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?rettype=fasta&"<<
-           "retmode=text&retstart=0&retmax=1&db=#{db}&query_key=#{queryKey}&WebEnv=#{webEnv}"
+           "retmode=text&retstart=0&retmax=1&db=#{dbtype}&query_key=#{queryKey}&WebEnv=#{webEnv}"
         result = Net::HTTP.get(URI.parse(uri))
 
         #parse FASTA output
@@ -91,15 +92,16 @@ class Sequence
         header        = rec[0..nl-1]
         seq           = rec[nl+1..-1]
         @raw_sequence = seq.gsub!(/\n/,'')
-        unless  @raw_sequence.index(/ERROR/) == nil
+        unless @raw_sequence.index(/ERROR/) == nil
           @raw_sequence = ""
         end
       end
       @raw_sequence
-      rescue Exception => error
-  #      @raw_sequence = ""
-      end
+    rescue Exception => error
+#      @raw_sequence = ""
+    end
   end
+
 
   ##
   # Initializes the corresponding attribute of the sequence
@@ -107,7 +109,7 @@ class Sequence
   def init_tabular_attribute(column, value)
     case column
       when "sseqid"
-        #@definition = value   
+        #@definition = value
         @identifier = value
       when "qseqid"
         #@definition = value
@@ -115,7 +117,7 @@ class Sequence
       when "sacc"
         @accession_no = value
       when "slen"
-        @length_protein = value.to_i  
+        @length_protein = value.to_i
     end
   end
 
