a3b3e9bef98763ae16db80491d093ac63ada8057
Anurag Priyam <anurag08priyam@gmail.com>
2014-11-12 09:27:08 +0530

    Remove some dead code.
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- lib/genevalidator/blast.rb
+++ lib/genevalidator/blast.rb
@@ -57,51 +57,6 @@ class BlastUtils
     end
   end
 
-  # NOTE: The below method isn't currently being used...
-
-  ##
-  # Calls blast from file with specific parameters
-  # Param:
-  # +command+: blast command in String format (e.g 'blastx' or 'blastp')
-  # +filename+: name of the FAST file
-  # +query+: +String+ containing the the query in fasta format
-  # +gapopen+: gapopen blast parameter
-  # +gapextend+: gapextend blast parameter
-  # +db+: database
-  # Output:
-  # String with the blast xml output
-  def self.call_blast_from_file(command, filename, gapopen, gapextend, db="nr -remote")
-    begin  
-      raise TypeError unless command.is_a? String and filename.is_a? String
-
-      evalue = "1e-5"
-
-      #output = 5 (XML Blast output)
-      cmd = "#{command} -query #{filename} -db #{db} -evalue #{evalue} -outfmt 5 -gapopen #{gapopen} -gapextend #{gapextend} "
-      puts "Executing \"#{cmd}\"..."
-      puts "This may take a while..."
-      output = %x[#{cmd}          if xml_file == nil
-            file = File.open(xml_file, "rb").read
-            b.parse_xml_output(file)
-          end 2>/dev/null]
-
-      if output.empty?
-        raise ClasspathError.new      
-      end
-
-      return output
-
-    rescue TypeError => error
-      $stderr.print "Type error at #{error.backtrace[0].scan(/\/([^\/]+:\d+):.*/)[0][0]}. "<<
-        "Possible cause: one of the arguments of 'call_blast_from_file' method has not the proper type\n"      
-      exit
-    rescue ClasspathError =>error
-      $stderr.print "BLAST error at #{error.backtrace[0].scan(/\/([^\/]+:\d+):.*/)[0][0]}. "<<
-        "Did you add BLAST path to LOADPATH?\n"      
-      exit
-    end
-  end
-
   ##
   # Parses the next query from the blast xml output query
   # Params:
@@ -114,7 +69,6 @@ class BlastUtils
       raise TypeError unless iterator.is_a? Enumerator
 
       hits = Array.new
-      predicted_seq = Sequence.new
       iter = iterator.next
 
       # parse blast the xml output and get the hits
