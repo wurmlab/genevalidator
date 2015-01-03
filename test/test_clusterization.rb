94b1e3e67551d9ed7ad966f77f55fc83d7bd2c54
IsmailM <ismail.moghul@gmail.com>
2015-01-03 11:52:12 +0000

    fixing the requiring of the test helper

--- test/test_clusterization.rb
+++ test/test_clusterization.rb
@@ -1,4 +1,4 @@
-require 'test/test_helper'
+require './test_helper'
 require 'minitest/autorun'
 require 'genevalidator/clusterization'
 
--- test/test_enumerable.rb
+++ test/test_enumerable.rb
@@ -1,4 +1,4 @@
-require 'test/test_helper'
+require './test_helper'
 require 'genevalidator/enumerable'
 require 'minitest/autorun'
 
