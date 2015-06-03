c83ae66abb2e9496af6a6a04cdff4f463590df9a
IsmailM <ismail.moghul@gmail.com>
2015-06-03 01:42:21 +0100

    fix general tests

--- test/test_all_validations.rb
+++ test/test_all_validations.rb
@@ -3,93 +3,81 @@ require 'minitest/autorun'
 require 'yaml'
 require 'fileutils'
 require 'genevalidator'
-require 'genevalidator/blast'
-require 'genevalidator/validation_length_cluster'
-require 'genevalidator/validation_length_rank'
-require 'genevalidator/validation_blast_reading_frame'
-require 'genevalidator/validation_gene_merge'
-require 'genevalidator/validation_duplication'
-require 'genevalidator/validation_open_reading_frame'
-require 'genevalidator/validation_alignment'
 
 module GeneValidator
   class ValidateOutput < Minitest::Test
+    prot_dir    = 'test/test_files/all_validations_prot'
+    prot_input  = File.join(prot_dir, 'prot.fa')
+    prot_xml    = File.join(prot_dir, 'prot.blast_xml')
+    prot_tab    = File.join(prot_dir, 'prot.blast_tab6')
+    prot_raw    = File.join(prot_dir, 'prot.raw_seq')
 
-    prot_input_fasta_file  = "test/test_files/all_validations_prot/all_validations_prot.fasta"
-    prot_blast_xml_file    = "#{prot_input_fasta_file}.blast_xml"
-    prot_blast_xml_raw_seq = "#{prot_input_fasta_file}.blast_xml.raw_seq"
+    mrna_dir    = 'test/test_files/all_validations_mrna'
+    mrna_input  = File.join(mrna_dir, 'mrna.fa')
+    mrna_xml    = File.join(mrna_dir, 'mrna.blast_xml')
+    mrna_tab    = File.join(mrna_dir, 'mrna.blast_tab6')
+    mrna_raw    = File.join(mrna_dir, 'mrna.raw_seq')
 
-    prot_blast_tab_file    = "#{prot_input_fasta_file}.blast_tab"
-    prot_blast_tab_raw_seq = "#{prot_input_fasta_file}.blast_tab.raw_seq"
+    tab_options = 'qseqid sseqid sacc slen qstart qend sstart send length' \
+                  ' qframe pident nident evalue qseq sseq'
+    database    = 'swissprot -remote'
+    threads     = '1'
 
-    mrna_input_fasta_file  = "test/test_files/all_validations_mrna/all_validations_mrna.fasta"
-    mrna_blast_xml_file    = "#{mrna_input_fasta_file}.blast_xml"
-    mrna_blast_xml_raw_seq = "#{mrna_input_fasta_file}.blast_xml.raw_seq"
-
-    mrna_blast_tab_file    = "#{mrna_input_fasta_file}.blast_tab"
-    mrna_blast_tab_raw_seq = "#{mrna_input_fasta_file}.blast_tab.raw_seq"
-
-    tab_options            = "qseqid sseqid sacc slen qstart qend sstart send length qframe pident evalue"
-
-    database               = 'swissprot -remote'
-    threads                = '1'
-
-    # Unwanted Output Files 
-    prot_xml_out           = "#{prot_blast_xml_file}.out"
-    prot_tab_out           = "#{mrna_blast_tab_file}.out"
-    mrna_xml_out           = "#{mrna_blast_xml_file}.out"
-    mrna_tab_out           = "#{mrna_blast_tab_file}.out"
-    prot_output_dir        = "#{prot_input_fasta_file}.html"
-    mrna_output_dir        = "#{mrna_input_fasta_file}.html"
-    prot_yaml              = "#{prot_input_fasta_file}.yaml"
-    mrna_yaml              = "#{mrna_input_fasta_file}.yaml"
+    # Unwanted Output Files
+    prot_xml_out           = "#{prot_xml}.out"
+    prot_tab_out           = "#{prot_tab}.out"
+    prot_output_dir        = "#{prot_input}.html"
+    prot_json              = "#{prot_input}.json"
+    mrna_xml_out           = "#{mrna_xml}.out"
+    mrna_tab_out           = "#{mrna_tab}.out"
+    mrna_output_dir        = "#{mrna_input}.html"
+    mrna_json              = "#{prot_input}.json"
 
     describe 'Protein dataset' do
       it 'xml and tabular inputs give the same output' do
-
         original_stdout = $stdout.clone
         $stdout.reopen(prot_xml_out, 'w')
 
         FileUtils.rm_rf(prot_output_dir) rescue Error
-
         opts = {
-          validations: %w(lenc lenr frame merge dup orf),
+          validations: %w(lenc lenr frame merge dup orf align),
           db: database,
           num_threads: threads,
           fast: false,
-          input_fasta_file: prot_input_fasta_file,
-          blast_xml_file: prot_blast_xml_file,
-          raw_sequences: prot_blast_xml_raw_seq,
+          input_fasta_file: prot_input,
+          blast_xml_file: prot_xml,
+          raw_sequences: prot_raw,
           test: true
         }
 
-        (GeneValidator::Validation.new(opts, 1, false)).run
+        GeneValidator.init(opts, 1, false)
+        GeneValidator.run
         $stdout.reopen original_stdout
         $stdout.reopen(prot_tab_out, 'w')
 
         FileUtils.rm_rf(prot_output_dir) rescue Error
 
         opts1 = {
-          validations: %w(lenc lenr frame merge dup orf),
+          validations: %w(lenc lenr frame merge dup orf align),
           db: database,
           num_threads: threads,
           fast: false,
-          input_fasta_file: prot_input_fasta_file,
-          blast_tabular_file: prot_blast_tab_file,
+          input_fasta_file: prot_input,
+          blast_tabular_file: prot_tab,
           blast_tabular_options: tab_options,
-          raw_sequences: prot_blast_tab_raw_seq,
+          raw_sequences: prot_raw,
           test: true
         }
 
-        (GeneValidator::Validation.new(opts1, 1, false)).run
+        GeneValidator.init(opts1, 1, false)
+        GeneValidator.run
         $stdout.reopen original_stdout
 
         diff = FileUtils.compare_file(prot_xml_out, prot_tab_out)
 
         File.delete(prot_xml_out)
         File.delete(prot_tab_out)
-        File.delete(prot_yaml)
-
+        File.delete(prot_json)
         FileUtils.rm_rf(prot_output_dir)
 
         assert_equal(true, diff)
@@ -98,7 +86,6 @@ module GeneValidator
 
     describe 'mRNA dataset' do
       it 'xml and tabular inputs give the same output' do
-
         original_stdout = $stdout.clone
         $stdout.reopen(mrna_xml_out, 'w')
 
@@ -109,13 +96,14 @@ module GeneValidator
           db: database,
           num_threads: threads,
           fast: false,
-          input_fasta_file: mrna_input_fasta_file,
-          blast_xml_file: mrna_blast_xml_file ,
-          raw_sequences: mrna_blast_xml_raw_seq,
+          input_fasta_file: mrna_input,
+          blast_xml_file: mrna_xml,
+          raw_sequences: mrna_raw,
           test: true
         }
 
-        (GeneValidator::Validation.new(opts, 1, false)).run
+        GeneValidator.init(opts, 1, false)
+        GeneValidator.run
         $stdout.reopen original_stdout
         $stdout.reopen(mrna_tab_out, 'w')
 
@@ -126,21 +114,22 @@ module GeneValidator
           db: database,
           num_threads: threads,
           fast: false,
-          input_fasta_file: mrna_input_fasta_file,
-          blast_tabular_file: mrna_blast_tab_file,
+          input_fasta_file: mrna_input,
+          blast_tabular_file: mrna_tab,
           blast_tabular_options: tab_options,
-          raw_sequences: mrna_blast_tab_raw_seq,
+          raw_sequences: mrna_raw,
           test: true
         }
 
-        (GeneValidator::Validation.new(opts1, 1, false)).run
+        GeneValidator.init(opts1, 1, false)
+        GeneValidator.run
         $stdout.reopen original_stdout
 
         diff = FileUtils.compare_file(mrna_xml_out, mrna_tab_out)
 
         File.delete(mrna_xml_out)
         File.delete(mrna_tab_out)
-        File.delete(mrna_yaml)
+        File.delete(mrna_json)
 
         FileUtils.rm_rf(mrna_output_dir)
 
